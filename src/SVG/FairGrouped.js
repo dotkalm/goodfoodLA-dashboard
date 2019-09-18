c6ef362a28c68fdaf8a4a616bd32517865b40440
dotkalm <joelmholmberg@gmail.com>
2019-09-18 15:05:01 -0700

    svg label

--- src/SVG/FairGrouped.js
+++ src/SVG/FairGrouped.js
@@ -6,7 +6,7 @@ const FairGrouped = () => {
 
     return (
         <FairStyle>
-        Fair
+        <p>Fair</p>
 <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
 	 viewBox="0 0 208 190"  >
 
