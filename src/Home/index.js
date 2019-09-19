5534616bef17033b23b84800027ffa88162f53d5
Jason Wong <jwong175@hotmail.com>
2019-09-18 17:40:39 -0700

    Home page content and styling

--- src/Home/index.js
+++ src/Home/index.js
@@ -9,14 +9,26 @@ import SustainableGrouped from '../SVG/SustainableGrouped'
 const HealthyGroupSvg = (props) => {
     console.log('inside healthy group')
     return (
-        <DivContainer>
+        <div className="homeFlex">
+            <span id="homeText">The Food System Dashboard utilizes both quantitative and qualitative data to evaluate progress in achieving a healthier, greener and more equitable food system in Los Angeles. The Los Angeles Food Policy Council produced the Food System Dashboard for organizations, agencies, policymakers and the public to guide our strategy and measure impact in achieving “Good Food for All.”</span>
+            <span id="homeTitle"><h2>What Do We Mean By Good Food?</h2></span>
+            <DivContainer>
             <DivStyle>
                 <HealthyGrouped/>
                 <AffordableGrouped/> 
                 <SustainableGrouped/> 
                 <FairGrouped/>
             </DivStyle>
-        </DivContainer>
+          </DivContainer>
+              <span id="homeTitle"><h2>What is a Good Food System?</h2></span>
+              <ul id="list">
+                <li> Prioritizes the health and wellbeing of our residents</li>
+                <li> Makes healthy, high quality food affordable</li>
+                <li> Contributes to a thriving economy where all participants in the food supply chain receive fair compensation and fair treatment</li>
+                <li> Protects and strengthens our biodiversity and regenerates natural resources</li>
+                <li> Ensures that Good Food is accessible to all</li>
+              </ul>
+        </div>
     )
 
 
